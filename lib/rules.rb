97466b3720fa0b3aa4713cb73f66a69a5a9cb6c5
ksilin <konstantin.silin@gmail.com>
2013-12-17 21:13:52 +0100

    simplified rule switching and application

--- lib/rules.rb
+++ lib/rules.rb
@@ -3,37 +3,35 @@ class Rules
   # rules in B[]S[] format
   # default rule: B3S23
 
-  RULES = {:original => [[3], [2, 3]],
-           :highlife => [[3, 6], [2, 3]],
-           :seeds => [[2], []],
-           :live_free_or_die => [[2], [0]],
-           :dotlife => [[3], [0, 2, 3]],
-           :mazectric => [[3], [1, 2, 3, 4]],
-           :coral => [[3], [4, 5, 6, 7, 8]],
-           :maze => [[3], [1, 2, 3, 4, 5]],
+  RULES = {:original => { :dead => [3], :alive => [2, 3]},
+           :highlife => { :dead => [3, 6], :alive => [2, 3]},
+           :seeds => { :dead => [2], :alive => []},
+           :live_free_or_die => { :dead => [2], :alive => [0]},
+           :dotlife => { :dead => [3], :alive => [0, 2, 3]},
+           :mazectric => { :dead => [3], :alive => [1, 2, 3, 4]},
+           :coral => { :dead => [3], :alive => [4, 5, 6, 7, 8]},
+           :maze => { :dead => [3], :alive => [1, 2, 3, 4, 5]},
   }
 
   ODD_RULES = {
-      :gnarl => [[1], [1]],
-      :replicator => [[1, 3, 5, 7], [1, 3, 5, 7]],
-      :fredkin => [[1, 3, 5, 7], [0, 2, 4, 6, 8]],
+      :gnarl => { :dead => [1], :alive => [1]},
+      :replicator => { :dead => [1, 3, 5, 7], :alive => [1, 3, 5, 7]},
+      :fredkin => { :dead => [1, 3, 5, 7], :alive => [0, 2, 4, 6, 8]},
   }
 
   def initialize(rule = :original)
-    @rule = rule
+    @rule = RULES[rule]
   end
 
   def next
-    @rule = RULES.keys[(RULES.keys.index(@rule) + 1)%RULES.keys.size]
-    p "switching to #{@rule}"
+    v = RULES.values
+    @rule = v[(v.index(@rule) + 1)%v.size]
+    p "switching to #{RULES.key(@rule)}"
   end
 
   def apply(cell, alive_neighbors)
-    case cell.state
-      when :alive
-        cell.die unless RULES[@rule][1].include? alive_neighbors
-      when :dead
-        cell.live if RULES[@rule][0].include? alive_neighbors
-    end
+
+    live_or_die = @rule[cell.state].include?(alive_neighbors) ? :live : :die
+    cell.send (live_or_die)
   end
 end
